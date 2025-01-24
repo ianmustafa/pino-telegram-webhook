546d34e35677ea89095cd4ab9182fa24d87fd3bb
Evgeniy Politiko <jhon-mosk@yandex.ru>
2025-01-24 13:51:11 +0300

    new formatting

--- test/transport.test.js
+++ test/transport.test.js
@@ -1,21 +1,22 @@
-import assert from "node:assert/strict";
-import { Transform } from "node:stream";
-import { after, describe, it } from "node:test";
-import { MockAgent, setGlobalDispatcher } from "undici";
-import createTransport, { sendMsgToTg } from "../index.js";
+/* eslint-disable camelcase */
+import assert from 'node:assert/strict';
+import { Transform } from 'node:stream';
+import { after, describe, it } from 'node:test';
+import { MockAgent, setGlobalDispatcher } from 'undici';
+import createTransport, { sendMsgToTg } from '../index.js';
 
-const API_URL = "https://api.telegram.org";
+const API_URL = 'https://api.telegram.org';
 const chatId = -1234567890;
 const incorrectChatId = -9876543210;
-const botToken = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11";
-const incorrectBotToken = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew12";
-const text = "Test message";
-const incorrectText = "?/:,*~@#$%^&()-+=_<.>|";
+const botToken = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11';
+const incorrectBotToken = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew12';
+const text = 'Test message';
+const incorrectText = '?/:,*~@#$%^&()-+=_<.>|';
 const extra = {
-  parse_mode: "HTML",
+  parse_mode: 'HTML',
 };
 const incorrectExtra = {
-  parse_mode: "hHtml",
+  parse_mode: 'hHtml',
 };
 
 const mockAgent = new MockAgent({ connections: 1 });
@@ -26,11 +27,11 @@ const mockClient = mockAgent.get(API_URL);
 mockClient
   .intercept({
     path: `/bot${botToken}/sendMessage`,
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       chat_id: chatId,
-      text: text,
+      text,
       ...extra,
     }),
   })
@@ -39,93 +40,93 @@ mockClient
 mockClient
   .intercept({
     path: `/bot${botToken}/sendMessage`,
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       chat_id: incorrectChatId,
-      text: text,
+      text,
       ...extra,
     }),
   })
-  .reply(400, "Bad Request");
+  .reply(400, 'Bad Request');
 
 mockClient
   .intercept({
     path: `/bot${incorrectBotToken}/sendMessage`,
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       chat_id: chatId,
-      text: text,
+      text,
       ...extra,
     }),
   })
-  .reply(401, "Unauthorized");
+  .reply(401, 'Unauthorized');
 
 mockClient
   .intercept({
     path: `/bot${botToken}/sendMessage`,
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       chat_id: chatId,
       text: incorrectText,
       ...extra,
     }),
   })
-  .reply(400, "Bad Request");
+  .reply(400, 'Bad Request');
 
 mockClient
   .intercept({
     path: `/bot${botToken}/sendMessage`,
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       chat_id: chatId,
-      text: text,
+      text,
       ...incorrectExtra,
     }),
   })
-  .reply(400, "Bad Request");
+  .reply(400, 'Bad Request');
 
-describe("pino-telegram-webhook", () => {
-  it("build transport", async () => {
+describe('pino-telegram-webhook', () => {
+  it('build transport', async () => {
     const transport = await createTransport({ chatId, botToken, extra });
-    assert.ok(transport instanceof Transform, "Failed to build transport.");
+    assert.ok(transport instanceof Transform, 'Failed to build transport.');
   });
 
-  describe("send message to telegram", () => {
+  describe('send message to telegram', () => {
     after(async () => await mockClient.close());
 
-    it("wrong chat ID", () => {
+    it('wrong chat ID', () => {
       assert.rejects(
         async () => await sendMsgToTg(incorrectChatId, botToken, text, extra),
-        new Error("400: Bad Request")
+        new Error('400: Bad Request'),
       );
     });
 
-    it("wrong bot token", () => {
+    it('wrong bot token', () => {
       assert.rejects(
         async () => await sendMsgToTg(chatId, incorrectBotToken, text, extra),
-        new Error("401: Unauthorized")
+        new Error('401: Unauthorized'),
       );
     });
 
-    it("wrong text", () => {
+    it('wrong text', () => {
       assert.rejects(
         async () => await sendMsgToTg(chatId, botToken, incorrectText, extra),
-        new Error("400: Bad Request")
+        new Error('400: Bad Request'),
       );
     });
 
-    it("wrong extra", () => {
+    it('wrong extra', () => {
       assert.rejects(
         async () => await sendMsgToTg(chatId, botToken, text, incorrectExtra),
-        new Error("400: Bad Request")
+        new Error('400: Bad Request'),
       );
     });
 
-    it("success sent", async () => {
+    it('success sent', async () => {
       await sendMsgToTg(chatId, botToken, text, extra);
     });
   });
